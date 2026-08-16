0884b4e01105f872b1d20d817f59b4d92873c902
Marcel Meyer <23430036+marcel-b-meyer1987@users.noreply.github.com>
2026-08-16 11:04:06 +0200

    adjust layout, spacing, size and overflow properties of ToDoDetails to improve mobile UX

--- templates/ToDoDetail.js
+++ templates/ToDoDetail.js
@@ -82,8 +82,10 @@ export class ToDoDetail {
             <!-- Checklist -->
             <div class="two-col-details-container" id="checklist-container">
                 <span><strong>&#9745</strong></span>
-                <span class="todo-details-label checklist-link">${labels.checklist}</span>
-                <span class="todo-details-label checklist-counter">(${todo.checklist.length})</span>
+                <div>
+                    <span class="todo-details-label checklist-link">${labels.checklist}</span>
+                    <span class="todo-details-label checklist-counter">(${todo.checklist.length})</span>
+                </div>
             </div>
 
             <!-- Categories -->
